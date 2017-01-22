15ef490edf960d027e28d798f24729915c1a50df
Andre Köpke <andre.koepke@haw-hamburg.de>
2017-01-22 20:45:26 +0100

    add (inital) contains basic code

---
+++ scripts/config.php
@@ -0,0 +1,12 @@
+<?php
+	define ('DB_USER', "fez");
+	define ('DB_PASS', "b7Z3JAFK5UJp7Axv");
+	define ('DB_DATABASE', "flottbek");
+	define ('DB_HOST', "localhost");
+
+	define('BOT_TOKEN', '254513152:AAGWdPHeUlhM3Gq8-S50fqr11cP9qOZJF-A');
+	define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+	define('KEY', '123456789');
+	define('WEBHOOK_URL', 'https://f1925.ddns.net/scripts/incoming.php');
+
+?>
